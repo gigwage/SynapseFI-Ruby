d1489afa7618162f31e1b6b6300f3c2dc03895a7
ben mercer <benmercerdev@gmail.com>
2024-03-14 09:45:24 -0600

    Add logger to payload creator

--- lib/synapse_pay_rest/models/transaction/batch_transaction.rb
+++ lib/synapse_pay_rest/models/transaction/batch_transaction.rb
@@ -94,6 +94,7 @@ module SynapsePayRest
         # new fee flow
         fees = options[:fees] if options[:fees]
         payload['fees'] = fees if fees.any?
+        Rails.logger.info "payload_for_transaction: #{payload}"
         payload
       end
 
